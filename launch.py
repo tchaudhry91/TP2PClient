da9e2850aa4fb5107955474edb12af7e43a0c32f
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2012-12-01 17:15:02 +0530

    Cleaned Up File Send/Request

--- launch.py
+++ launch.py
@@ -1,6 +1,17 @@
+import sys
 from fileListing import generate_fl
+from fileTransfer import fileServer, clientPrompt
+from twisted.internet import reactor
+  
 
-if __name__=="__main__":
-    generate_fl.generate_fl("/home/tanmay/TEST","tuxybuzz")
+if __name__=="__main__": 
+    root_dir = sys.argv[1]
+    port = 9876
+    
+    generate_fl.generate_fl(root_dir,"tuxybuzz")
     print("File Listing Created..")
+    
+    fileServer.startServer(root_dir, port)
+    reactor.callWhenRunning(clientPrompt.startPrompt)
+    reactor.run()
     
